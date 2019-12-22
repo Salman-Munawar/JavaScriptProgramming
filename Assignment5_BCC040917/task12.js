20f629a457c9b1cf1d135c9af9d9413cbfac3bc1
Areeba Saqib <areebasaqib15@gmail.com>
2019-12-22 16:36:21 +0500

    Assignment5 Completed

--- Assignment5_BCC040917/task12.js
+++ Assignment5_BCC040917/task12.js
@@ -1,12 +1,45 @@
 function guessNumber() {
     // Get a random integer from 1 to 10 inclusive
-    var num = Math.ceil(Math.random() * 10);
-    var guess;
+    // random value generated 
+    var randomNum = Math.floor(Math.random() * 10 + 1); 
+    var counter = 0;
+    var attempts = 3;
+   // var guess = prompt('Guess the number between 1 and 10');
+
+    var guess;    
    
-    while (guess != num) {
-      guess = prompt('Guess the number between 1 and 10 inclusive');
-    }
-    alert('Congratulations!\nThe number was ' + num);
-  }
-   
+    
+    while (guess != randomNum) {
+      guess = prompt('Guess the number between 1 and 10');
+      counter ++ ;
+      
+      if(counter > attempts){
+        
+        document.write("You Lose. Press F5 to play  again");
+        break;
+      }
+
+      if(guess == randomNum){
+        document.write('Congratulations! You are right, You won\nThe Correct Number was ' + randomNum);
+        document.write("<p> It Took you " + counter + " Attempts  to get  the Correct Number</p>");      
+
+      }      
+    }  
+}
   guessNumber();
+
+  // var play = prompt("Are you want to play again");
+  //     if(play.toLowerCase() == "yes" || play.toLowerCase() == "y" )
+  //     {
+  //        guessNumber();
+  //     }
+  //     else
+  //     {
+  //        document.write("Thank you for playing with us today");
+  //     }
+
+
+ 
+  
+ 
+ 
